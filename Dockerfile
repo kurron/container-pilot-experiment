f5d29b5bb3ad06f4b518021e2ff18b601e9fa157
Ron Kurr <kurr@kurron.org>
2017-04-12 13:36:11 -0400

    Can build upon the Zulu JDK

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
-FROM kurron/docker-oracle-jdk-8
+FROM azul/zulu-openjdk:8u121
 
 MAINTAINER Ron Kurr "kurr@jvmguy.com"
 
-EXPOSE 8080
+#EXPOSE 8080
 
-# switching to admin level user
-USER root
+#ENTRYPOINT ["java", "-server", "-Xms128m", "-Xmx128m", "-Djava.awt.headless=true",  "-jar", "/opt/echo.jar"]
 
-ENTRYPOINT ["java", "-server", "-Xms128m", "-Xmx128m", "-Djava.awt.headless=true",  "-jar", "/opt/echo.jar"]
+#COPY build/libs/echo-0.0.0.RELEASE.jar /opt/echo.jar
 
-COPY build/libs/echo-0.0.0.RELEASE.jar /opt/echo.jar
+CMD ["java", "-version"]
--- tag-and-push.sh
+++ tag-and-push.sh
@@ -4,10 +4,10 @@
 UNIXTIME=$(date +%s)
 
 # docker tag SOURCE_IMAGE[:TAG] TARGET_IMAGE[:TAG]
-docker tag springcloudawsecho_echo:latest kurron/spring-cloud-aws-echo:latest
-docker tag springcloudawsecho_echo:latest kurron/spring-cloud-aws-echo:${UNIXTIME}
+docker tag containerpilotexperiment_container-pilot:latest kurron/container-pilot:latest
+docker tag containerpilotexperiment_container-pilot:latest kurron/container-pilot:${UNIXTIME}
 docker images
 
 # Usage:  docker push [OPTIONS] NAME[:TAG]
-docker push kurron/spring-cloud-aws-echo:latest
-docker push kurron/spring-cloud-aws-echo:${UNIXTIME}
+docker push kurron/container-pilot:latest
+docker push kurron/container-pilot:${UNIXTIME}
